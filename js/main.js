ba2da46249d0dc0833d6df009036177536c8309b
Maciej Gałązka <maciejtgalazka@gmail.com>
2024-05-09 22:23:22 +0200

    navitems animation

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ const handleNav = () => {
 			nav.classList.remove('nav-active')
 		})
 	})
+
+	handleNavItemsAnimation() //animacja opóźniająca pokazywanie się linków w nawigacji
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's' //opóźnienie animacji
+		delayTime++
+	})
 }
 
 navBtn.addEventListener('click', handleNav)
